c86a1cbb5b2b27c74c0b108248195f67847fafe0
Rohini19-coder <gaddamrohini7@gmail.com>
2025-05-27 19:34:47 +0530

    Load products from the backend

--- scriptss/amazon.js
+++ scriptss/amazon.js
@@ -1,6 +1,9 @@
 import {cart,addToCart} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {products,loadProducts} from '../data/products.js';
 import formatCurrency from './utils/money.js';
+loadProducts(renderProductsGrid);
+function renderProductsGrid(){
+
 let productsHTML = '';
 products.forEach((product)=>{
     productsHTML += `
@@ -81,3 +84,4 @@ document.querySelector('.js-products-grid').innerHTML
             
         });
       });
+    }
